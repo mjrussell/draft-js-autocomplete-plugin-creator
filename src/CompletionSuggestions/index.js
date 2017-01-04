6525f6238809b8f3642bcfee8243808e9de025ad
Michael Wegman <mwegman@cisco.com>
2017-01-04 15:09:51 -0700

    fix(dropdown): 'enter' key selects item from drop down

--- src/CompletionSuggestions/index.js
+++ src/CompletionSuggestions/index.js
@@ -210,7 +210,7 @@ export default function (addModifier, Entry, suggestionsThemeKey) {
 
     commitSelection = () => {
       this.onCompletionSelect(this.props.suggestions.get(this.state.focusedOptionIndex));
-      return true;
+      return 'handled';
     };
 
     openDropdown = () => {
